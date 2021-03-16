ea966af22bdf105b6ac97968af3f585830f30305
Y0u9h <61617776+iMayday-Yee@users.noreply.github.com>
2021-03-16 23:44:32 +0800

    Delete example directory

--- example/3/edge.cpp
+++
@@ -1,9 +0,0 @@
-#include "edge.h"
-
-Edge::Edge(int begin_tag,int end_tag,double distance,int lineNum)
-{
-    this->begin_tag = begin_tag;
-    this->end_tag = end_tag;
-    this->distance = distance;
-    this->lineNum = lineNum;
-}
